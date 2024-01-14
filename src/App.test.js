896ebfd16bb354b8cacca673497ffa73c0ace759
TanyaSahdeo22 <tanyasahdeo98@gmail.com>
2024-01-14 16:37:47 +0100

    deleting old files

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('Availability of Task Tracker app', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Task Tracker/i);
-  expect(linkElement).toBeInTheDocument();
-});
